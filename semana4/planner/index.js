68cbbfe64e7187be6380202d1ffdfb8b43084086
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-06-09 14:29:56 -0300

    Semana 4 aula 14 exercício obrigatório

---
+++ semana4/planner/index.js
@@ -0,0 +1,32 @@
+function criarTarefa() {
+    const meuInput = document.getElementById("novaTarefaTexto") //coleta a tarefa e guarda na variável
+    const diaDaSemana = document.getElementById("diaDaSemana") //coleta o dia da semana e guarda na variável
+    const tarefa = meuInput.value //guardo o valor em uma variável
+    const dia = diaDaSemana.value //guardo o valor em uma variável
+
+    switch (dia) {
+        case "segunda":
+            // segunda.innerHTML += `<li> ${tarefa} </li>` //para referência futura
+            document.getElementById("segunda").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "terca":
+            document.getElementById("terca").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "quarta":
+            document.getElementById("quarta").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "quinta":
+            document.getElementById("quinta").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "sexta":
+            document.getElementById("sexta").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "sabado":
+            document.getElementById("sabado").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+        case "domingo":
+            document.getElementById("domingo").firstElementChild.innerHTML += `<li> ${tarefa} </li>`;
+            break;
+    }
+    meuInput.value = ""
+}
