f7103587c98277c7b74d60ce39f8222621b22893
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-06-12 13:17:54 -0300

    Exercícios de Lógica de Programação 1.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,3 +6,38 @@
 // 3. Tamanho total do array original, tamanho do array1(pares), tamanho array2(impares).
 // 4. Dompara todos os numeros menores que infito no array e grava, nesse caso todos, sendo o último 1590. 
 //  Depois grava o último maior número, neste caso termina em 1590.
+
+// Exercícios de Lógica de Programação:
+// 1. forEach:
+// const frutas = [
+// 	{nome : 'banana', peso : 0}
+// 	{nome : 'laranja', peso : 0}
+// ]
+
+// frutas.forEach((cadaFruta, i, a) => {
+// 	cadaFruta.peso = 1
+// })
+
+// map:
+// const frutas = [
+// 	{nome : 'banana', peso : 0}
+// 	{nome : 'laranja', peso : 0}
+// ]
+
+// frutas.forEach((cadaFruta, i, a => {
+// 	cadaFrura.peso = 1
+// }
+// const minhaFeira = frutas.map((cadaFruta, i, a) => {
+// 	return fruta.peso
+// })
+
+// filter:
+// const frutas = [
+// 	{nome : 'banana', peso : 0}
+// 	{nome : 'laranja', peso : 0}
+// ] 
+// const minhaFeira = frutas.filter((cadaFruta, i, a) => {
+// 	if (fruta.nome === banana){
+// 		fruta.peso += 1
+// 		return fruta.peso
+// })
