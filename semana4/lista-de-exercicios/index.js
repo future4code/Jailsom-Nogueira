36b28aa3f200f1363e430e5000f48b1fdae860ce
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-06-12 13:16:13 -0300

    exercicios de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+// Exercícios de interpretação de código:
+// 1. Multiplica 100 dólares pela cotação dada pelo usuário e imprime o valor em reais equivalente aos mesmos U$100 
+//  nesta cotação informada.
+// 2. Multiplica o valor em ações no Switch case e o valor de "Tesouro direto" não será processado por não estar 
+//  na condição. Neste caso o valor "Default" será apresentado.
+// 3. Tamanho total do array original, tamanho do array1(pares), tamanho array2(impares).
+// 4. Dompara todos os numeros menores que infito no array e grava, nesse caso todos, sendo o último 1590. 
+//  Depois grava o último maior número, neste caso termina em 1590.
