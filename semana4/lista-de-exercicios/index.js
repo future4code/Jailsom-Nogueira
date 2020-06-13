5197cd762200348e6636f0deb3deb5a8c9222fed
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-06-13 16:27:36 -0300

    Exercícios de Funções de array 4a

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -313,3 +313,30 @@
 //     }
 // })
 // console.log(quemNaoEntra)
+
+// 4.
+// a.
+// const consultas = [
+//     { nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+//     { nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+//     { nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+//     { nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+// ]
+
+// const todasConsultas = consultas.filter((aConsulta, i, a) => {
+//     if (aConsulta.genero === "masculino") {
+//         aConsulta.genero = "Sr."
+//         aConsulta.lembrar = "lembrá-lo"
+//     } else {
+//         aConsulta.genero = "Sra."
+//         aConsulta.lembrar = "lembrá-la"
+//     }
+
+//     if (aConsulta.cancelada === false) {
+//         aConsulta.email = "Olá, " + aConsulta.genero + aConsulta.nome + ".Estamos enviando esta mensagem para " + aConsulta.lembrar + " da sua consulta no dia " + aConsulta.dataDaConsulta + ".Por favor, acuse o recebimento deste e-mail."
+//     } else {
+//         aConsulta.email = "Olá, " + aConsulta.genero + aConsulta.nome + ".Infelizmente, sua consulta marcada para o dia " + aConsulta.dataDaConsulta + " foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la"
+//     }
+//     return aConsulta
+// })
+// console.log(todasConsultas)
