94ea23aec1a879e4d2bb052e70ffa17d54095dd6
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-06-23 19:27:39 -0300

    semana 6 aula 22 exercicio 1

---
+++ semana6/boiler-plate-aula22/src/App.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import styled from 'styled-components'
+import './styles.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+class App extends React.Component {
+    state = {
+      tarefas: [],
+      inputValue: '',
+      filter: ''
+    }
+
+  componentDidUpdate() {
+
+  };
+
+  componentDidMount() {
+
+  };
+
+  onChangeInput = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  criaTarefa = () => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: this.state.inputValue,
+      completa: false
+    };
+    const listaDeTarefas = [...this.state.tarefas, novaTarefa]
+    this.setState({tarefas: listaDeTarefas})
+  }
+
+  selectTarefa = (id) => {
+    const tarefaCompleta = this.state.tarefas.filter((cadaTarefa) =>{//passa pelo array de tarefas, copiando para tarefaCompleta
+      if(id === cadaTarefa.id){ //se o id clicado é o mesmo que o itinerado 
+        this.setState({completa: true}) //transorme completa em true
+      }//se não, faça nada 
+    })
+    this.setState({tarefas: tarefaCompleta}) //devolve a lista completa já com as alterações para o array de tarefas
+  }
+
+  onChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const listaFiltrada = this.state.tarefas
+      .filter(tarefa => {
+        switch (this.state.filter) {
+          case 'pendentes':
+            return !tarefa.completa
+          case 'completas':
+            return tarefa.completa
+          default:
+            return true
+        }
+      })
+      console.log(this.state.tarefas)
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <button onClick={this.criaTarefa}>Adicionar</button>
+        </InputsContainer>
+        <br/>
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+          
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
+
+export default App
