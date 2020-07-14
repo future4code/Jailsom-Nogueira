1811fc05ff1d7b1a0604ce2b1884a1c603a27653
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-07-14 13:08:09 -0300

    get matches funcionando

--- semana9/astromatch/src/components/AllMatches/AllMatches.js
+++ semana9/astromatch/src/components/AllMatches/AllMatches.js
@@ -7,22 +7,37 @@ const AllMatches = () => {
   const [myMatches, setMyMatches] = useState([]);
   
   useEffect(() => {
+    getMatches()
+  }, []);
+
+  const getMatches = () => {
     axios
-      .get(
-        'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/jailsom-nogueira-turing/matches'
-        )
-      .then( response => {
-        setMyMatches(response.data.matches);
-      })
-      .catch( err => {
-        alert(err.message);
-      })
-  })
+    .get(
+      'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/jailsom-nogueira-turing/matches'
+      )
+    .then( response => {
+      setMyMatches(response.data.matches);
+    })
+    .catch( err => {
+      alert(err.message);
+    })
+  }
 
   return(
     <AllMatchsContainer>
-      <MatchAvatar />
-      <span>Nome do crush</span>
+      <ul>
+        {myMatches.map(match => {
+          return(
+            <MatchAvatar key={match.id}>
+              <img src={match.photo} alt="Avatar" />
+              <span>{match.name}</span>
+            </MatchAvatar>
+          )
+        })}
+      </ul>
+
+      {/* <MatchAvatar />
+      <span>Nome do crush</span> */}
     </AllMatchsContainer>
   )
 }
--- semana9/astromatch/src/components/AllMatches/style.js
+++ semana9/astromatch/src/components/AllMatches/style.js
@@ -6,10 +6,14 @@ export const AllMatchsContainer = styled.div`
   padding: 8px;
 `;
 
-export const MatchAvatar = styled.div`
+export const MatchAvatar = styled.li`
   width: 3.5rem;
   height: 3.5rem;
   border-radius: 50%;
   background-color: gray;
   margin-right: 0.80rem;
+  img{
+    width: 3.5rem;
+    height: 3.5rem;
+  }
 `;
